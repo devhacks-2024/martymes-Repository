82970f325f27dd093cedf7ef951faff3d6df9e50
Ty Paragas <ty.paragas01@gmail.com>
2024-02-24 10:29:39 -0600

    Added last item asset and sprite stuff to shop

--- shop_setup.py
+++ shop_setup.py
@@ -21,16 +21,20 @@ num_2 = font.render('2', True, WHITE)
 num_2_rect = num_2.get_rect()
 num_2_rect.center = (900/2, 500/2 + 50)
 
-item_3 = None
+item_3 = pygame.sprite.Sprite()
+item_3.image = pygame.image.load("assets/youmuus.png")
+item_3.rect = item_3.image.get_rect()
+item_3.rect.center = (900/2 + 313, 500/2 - 100)
 num_3 = font.render('3', True, WHITE)
 num_3_rect = num_3.get_rect()
 num_3_rect.center = (900/2 + 225, 500/2 + 50)
 
-shop_items = pygame.sprite.Group((item_1, item_2))
+shop_items = pygame.sprite.Group((item_1, item_2, item_3))
 ITEM_1_BOUGHT = False
 ITEM_2_BOUGHT = False
 ITEM_3_BOUGHT = False
 
+# Loss and win screens
 screenL = pygame.sprite.Sprite()
 screenL.image = pygame.image.load("assets/loss_screen.png")
 screenL.rect = screenL.image.get_rect()
