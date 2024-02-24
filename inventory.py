30132e52792169824d2fc3b0a704a9583a002bff
Ty Paragas <ty.paragas01@gmail.com>
2024-02-23 20:39:08 -0600

    Inventory showing

--- inventory.py
+++ inventory.py
@@ -1,13 +1,13 @@
 import pygame
-class Inventory:
-  def __init__(self, items):
-    self.items = items # Group of sprites
+class Inventory(pygame.sprite.Sprite):
+  def __init__(self,pos):
+    super().__init__()
+    self.image = pygame.image.load("assets/inventory.png").convert_alpha()
+    self.rect = self.image.get_rect()
+    self.rect.center = pos
 
   def remove_item(self, key):
     self.items.remove(key)
 
   def add_item(self, new_item):
     self.items.add(new_item)
-
-  def draw_sprites(self):
-    self.items.draw()
